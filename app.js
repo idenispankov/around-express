692edabb610b0f626bb19f1c6eb0b7e1adf81c37
Denis Pankov <idenispankov@gmail.com>
2021-02-17 20:19:51 -0800

    app.js

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 const express = require('express');
-const { PORT = 3000 } = process.env;
 const path = require('path');
+
+const { PORT = 3000 } = process.env;
 const app = express();
 
-
 app.use(express.static(path.join(__dirname, 'public')));
-
 
 app.listen(PORT, () => {
   console.log(`Server run on port ${PORT}`);
