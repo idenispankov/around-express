9ae92233d0b2b01385a2130fc9ac973560176b07
Denis Pankov <idenispankov@gmail.com>
2021-03-04 21:13:07 -0800

    user routes are all functional from database

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -6,23 +6,20 @@ const User = require("../models/user");
 const getUsers = (req, res) => {
   return User.find({})
     .then((users) => res.status(200).send(users))
-    .catch((err) => res.status(500).send({ message: "Error" }));
+    .catch((err) => res.status(500).send({ message: "Unable to find users" }));
 };
 
 const getSingleUser = (req, res) => {
-  return User.findById({ _id: req.params.id }).then((user) => {
-    if (user) {
-      return res.status(200).send(user);
-    }
-    return res.status(404).send({ message: "User ID not found" });
-  });
+  return User.findById({ _id: req.params.id })
+    .then((user) => res.send(user))
+    .catch((err) => res.status(500).send({ message: "User ID not found" }));
 };
 
 const createUser = (req, res) => {
   const { name, about, avatar } = req.body;
   return User.create({ name, about, avatar })
     .then((user) => res.send(user))
-    .catch((err) => res.status(500).send({ message: "Error" }));
+    .catch((err) => res.status(500).send({ message: "Unable to create User" }));
 };
 
 module.exports = {
@@ -30,23 +27,3 @@ module.exports = {
   getSingleUser,
   createUser,
 };
-
-// const getSingleUser = (req, res) => {
-//   getFileContent(usersData)
-//     .then((users) => {
-//       const user = users.find((user) => user._id === req.params.id);
-//       if (user) {
-//         return res.status(200).send(user);
-//       }
-//       return res.status(404).send({ message: 'User ID not found' });
-//     });
-// };
-
-// const getSingleUser = (req, res) => {
-//   return User.findById({ _id: req.params.id }).then((user) => {
-//     if (user) {
-//       return res.status(200).send(user);
-//     }
-//     return res.status(404).send({ message: "User ID not found" });
-//   });
-// };
