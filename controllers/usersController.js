76ad6ca02c90485b42dc4cba9d52640acdbb8a42
Denis Pankov <idenispankov@gmail.com>
2021-03-06 15:52:36 -0800

    user controller fully working

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -3,22 +3,22 @@ const User = require("../models/user");
 const getUsers = (req, res) => {
   return User.find({})
     .then((users) => res.status(200).send(users))
-    .catch((err) => res.status(400).send({ message: "Unable to find users" }));
+    .catch((err) => res.status(400).send({ message: err }));
 };
 
 const getSingleUser = (req, res) => {
   return User.findById({ _id: req.params.id })
     .then((user) => {
       if (!user) {
-        return res.status(400).send({ message: "User is not exist" });
+        return res.status(404).send({ message: "User Not Found" });
       }
       return res.status(200).send(user);
     })
     .catch((err) => {
       if (err.name === "CastError") {
-        return res.status(404).send({ message: "User is not exist" });
+        return res.status(404).send({ message: "User Not Found" });
       }
-      return res.status(500).send({ message: "Internal Server Error" });
+      return res.status(500).send(err.message);
     });
 };
 
@@ -30,18 +30,9 @@ const createUser = (req, res) => {
       if (err.name === "ValidationError") {
         return res.status(400).send({ message: err.message });
       }
-      return res.status(500).send({ message: "Internal Server Error" });
+      return res.status(500).send({ message: err.message });
     });
 };
-
-// const createUser = (req, res) => {
-//   const { name, about, avatar } = req.body;
-//   return User.create({ name, about, avatar }).then((user) =>
-//     res.status(200).send(user)
-//   );
-//   // .catch((err) => res.status(400).send({ message: "Unable to create User" }))
-//   // .catch((err) => res.status(500).send({ message: "Server Error" }));
-// };
 
 const updateUser = (req, res) => {
   const { name, about } = req.body;
@@ -52,11 +43,11 @@ const updateUser = (req, res) => {
   )
     .then((user) => {
       if (!user) {
-        res.status(404).send({ message: "User Not Exist" });
+        res.status(404).send({ message: "User Not Found" });
       }
       res.status(200).send(user);
     })
-    .catch((err) => res.status(500).send({ message: "Unable to update User" }));
+    .catch((err) => res.status(500).send({ message: err.message }));
 };
 
 function updateAvatar(req, res, next) {
@@ -72,9 +63,7 @@ function updateAvatar(req, res, next) {
       }
       res.status(200).send(user);
     })
-    .catch((err) =>
-      res.status(500).send({ message: "Unable to update avatar" })
-    );
+    .catch((err) => res.status(500).send({ message: err.message }));
 }
 
 module.exports = {
