931af9f8f2c7c3c812e76fff7120f5db736c546a
Denis Pankov <idenispankov@gmail.com>
2021-04-11 17:51:17 -0700

    cleanup app, will replace

--- controllers/notFoundController.js
+++
@@ -1,5 +0,0 @@
-const notFound = (req, res) => {
-  res.status(404).send({ message: 'Requested resource not found' });
-};
-
-module.exports = { notFound };
--- routes/notFound.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-
-const router = express.Router();
-const { notFound } = require("../controllers/notFoundController");
-
-// Non Existing page request
-router.get("*", notFound);
-
-module.exports = router;
